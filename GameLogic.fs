37b2abb6aae87d8ca870311df8fbe03350ccd64e
Luke Merrett <lukeamerrett@gmail.com>
2016-05-02 17:31:35 +0100

    Class for tracking years, seasons and phases

---
+++ GameLogic.fs
@@ -0,0 +1,42 @@
+﻿module GameLogic
+
+open Seasons
+
+type YearTracker() =
+    let mutable currentYear = {Year.year=1901; seasons=[spring; fall]}
+    let mutable currentSeason = 0
+    let mutable currentPhase = 0
+
+    let seasonsAvailable = 
+        currentYear.seasons.Length
+
+    let phasesAvailable = 
+        currentYear.seasons.Item(currentSeason).phases.Length
+
+    let incrementYear = 
+        currentYear.year <- currentYear.year + 1
+        currentSeason <- 0
+        currentPhase <- 0
+
+    let incrementSeason = 
+        if currentSeason >= seasonsAvailable then incrementYear
+        else (
+            currentSeason <- currentSeason + 1
+            currentPhase <- 0
+        )
+
+    let incrementPhase =
+        currentPhase <- currentPhase + 1
+
+    member this.EndTurn = 
+        if currentPhase + 1 >= phasesAvailable then incrementSeason
+        else incrementPhase
+
+    member this.GetCurrentYear = 
+        currentYear.year
+
+    member this.GetCurrentSeason =
+        currentYear.seasons.Item(currentSeason)
+
+    member this.GetCurrentPhase = 
+        this.GetCurrentSeason.phases.Item(currentPhase)
--- Seasons.fs
+++ Seasons.fs
@@ -4,5 +4,7 @@ open Domain
 
 type Season = {name:string; phases:Phase list}
 
+type Year = {mutable year:int; seasons:Season list}
+
 let spring = {Season.name="Spring"; phases=[Order; Retreat]}
 let fall = {Season.name="Fall"; phases=[Order; Retreat; Build]}
