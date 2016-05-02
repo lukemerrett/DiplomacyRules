0ed62f06d251bec80ec97731319137fd57bcc8d1
Luke Merrett <lukeamerrett@gmail.com>
2016-05-02 17:54:35 +0100

    Fixed let signatures so they are methods not immutable values

--- GameLogic.fs
+++ GameLogic.fs
@@ -1,6 +1,9 @@
 ﻿module GameLogic
 
+open Domain
 open Seasons
+
+type CurrentTurnDetails = {year:int; season:Season; phase:Phase}
 
 type YearTracker() =
     let mutable currentYear = {Year.year=1901; seasons=[spring; fall]}
@@ -10,33 +13,33 @@ type YearTracker() =
     let seasonsAvailable = 
         currentYear.seasons.Length
 
-    let phasesAvailable = 
+    let phasesAvailable() = 
         currentYear.seasons.Item(currentSeason).phases.Length
 
-    let incrementYear = 
+    let incrementYear() = 
         currentYear.year <- currentYear.year + 1
         currentSeason <- 0
         currentPhase <- 0
 
-    let incrementSeason = 
-        if currentSeason >= seasonsAvailable then incrementYear
+    let incrementSeason() = 
+        if currentSeason + 1 >= seasonsAvailable then incrementYear()
         else (
             currentSeason <- currentSeason + 1
             currentPhase <- 0
         )
 
-    let incrementPhase =
+    let incrementPhase() =
         currentPhase <- currentPhase + 1
 
-    member this.EndTurn = 
-        if currentPhase + 1 >= phasesAvailable then incrementSeason
-        else incrementPhase
+    member this.EndTurn() = 
+        if currentPhase + 1 >= phasesAvailable() then incrementSeason()
+        else incrementPhase()
 
-    member this.GetCurrentYear = 
-        currentYear.year
-
-    member this.GetCurrentSeason =
-        currentYear.seasons.Item(currentSeason)
-
-    member this.GetCurrentPhase = 
-        this.GetCurrentSeason.phases.Item(currentPhase)
+    member this.GetCurrentTurnDetails() =
+        let seasonToReturn = currentYear.seasons.Item(currentSeason)
+        let phaseToReturn = seasonToReturn.phases.Item(currentPhase)
+        {
+            CurrentTurnDetails.year=currentYear.year;
+            season=seasonToReturn;
+            phase=phaseToReturn
+        }
