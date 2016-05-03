109c55bcc0fa1820b4338e6a9e98fc92a7224c14
Luke Merrett <lukeamerrett@gmail.com>
2016-05-03 09:43:18 +0100

    Validation on whether a move is appropriate for a phase

---
+++ Validators.fs
@@ -0,0 +1,26 @@
+﻿module Validators
+
+open System
+open Domain
+open GameLogicTypes
+
+// List of validators to execute per move
+// Common interface (or F# alternative) so they can be executed
+// Alternatively, a list of functions to run all taking move and phase
+
+let moveIsValidForPhase (move:RequestedMove, turn:CurrentTurnDetails) = 
+    match turn.phase with
+        | Order -> match move.move with
+                        | MoveOrAttack _ -> true
+                        | Hold _ -> true
+                        | SupportMovingUnit _ -> true
+                        | SupportHoldingUnit _ -> true
+                        | Convoy _ -> true
+                        | _ -> false
+        | Retreat -> match move.move with
+                        | MoveOrAttack _ -> true
+                        | _ -> false
+        | Build -> match move.move with
+                        | Create _ -> true
+                        | Disband _ -> true
+                        | _ -> false
